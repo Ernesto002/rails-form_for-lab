d419bcb2d925907847482db2b380faacf6bba94a
ernesto <ernesto002@icloud.com>
2021-03-26 15:48:28 -0700

    commit tree 68cb6e58e35e3cf82671c98a976287ef390e75af

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :school_classes, only: [:index, :new, :show, :create, :update]
+  resources :students, only: [:index, :new, :show, :create, :update]
+  get 'school_classes/:id/edit', to: 'school_classes#edit', as: :edit_school_class
+  patch 'school_classes/:id', to: 'school_classes#update'
+  get 'students/:id/edit', to: 'students#edit', as: :edit_student
 end
